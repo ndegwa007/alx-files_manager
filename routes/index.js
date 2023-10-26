133658c836edbc4b73d8ce256c468232f2111cd7
ndegz <ndegwa8781@gmail.com>
2023-10-26 09:43:34 +0300

    files

--- routes/index.js
+++ routes/index.js
@@ -42,5 +42,31 @@ function controllerRouting(app) {
   router.post('/files', (req, res) => {
     FilesController.postUpload(req, res);
   });
+
+  // should retrieve the file document based on the ID
+  router.get('/files/:id', (req, res) => {
+    FilesController.getShow(req, res);
+  });
+
+  // should retrieve all users file documents for a
+  // specific parentId and with pagination
+  router.get('/files', (req, res) => {
+    FilesController.getIndex(req, res);
+  });
+
+  // should set isPublic to true on the file document based on the ID
+  router.put('/files/:id/publish', (req, res) => {
+    FilesController.putPublish(req, res);
+  });
+
+  // should set isPublic to false on the file document based on the ID
+  router.put('/files/:id/unpublish', (req, res) => {
+    FilesController.putUnpublish(req, res);
+  });
+
+  // should return the content of the file document based on the ID
+  router.get('/files/:id/data', (req, res) => {
+    FilesController.getFile(req, res);
+  });
 }
 export default controllerRouting;
